a5c243f039cabbe1d6021244c7bbc28d37a049ef
Tomasz Kłys <tomaszklys.tk@gmail.com>
2018-10-09 19:26:36 +0200

    Stworzenie defaultowego projektu konsolowego.

---
+++ StarCafe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StarCafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
